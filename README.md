d49473e77588009a61a45acfec25ece0dabfed28
shuangmin <605751789@qq.com>
2016-04-25 10:27:42 +0800

    eclipse 工程转换成 Android studio工程

--- README.md
+++
@@ -1,2 +0,0 @@
-# AndroidMQTT
-Android MQTT Demo
--- gen/org/eclipse/paho/android/service/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.eclipse.paho.android.service;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
